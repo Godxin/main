e13820fd93701d44110d7deecaba67b4c3e96a8b
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-13 22:27:06 +0800

    UniqueAppointmentList: import

---
+++ src/main/java/seedu/address/model/appointment/UniqueAppointmentList.java
@@ -0,0 +1,25 @@
+package seedu.address.model.appointment;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.model.person.exceptions.DuplicateAppointmentException;
+import seedu.address.model.person.exceptions.AppointmentNotFoundException;
+
+/**
+ * A list of appointments that enforces uniqueness between its elements and does not allow nulls.
+ *
+ * Supports a minimal set of list operations.
+ *
+ * @see Appointment#equals(Object)
+ * @see CollectionUtil#elementsAreUnique(Collection)
+ */
+public class UniqueAppointmentList {
+}
