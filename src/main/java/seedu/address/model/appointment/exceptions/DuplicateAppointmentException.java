4adc299fb0c6b211b12538e6a62aee9b469e734a
Godxin <35590237+Godxin@users.noreply.github.com>
2018-03-14 22:12:51 +0800

    Update DuplicateAppointmentException.java

--- src/main/java/seedu/address/model/appointment/exceptions/DuplicateAppointmentException.java
+++ src/main/java/seedu/address/model/appointment/exceptions/DuplicateAppointmentException.java
@@ -1,12 +1,1 @@
-package seedu.address.model.appointment.exceptions;
 
-import seedu.address.commons.exceptions.DuplicateDataException;
-
-/**
- * Signals that the operation will result in duplicate(clashed) appointment objects.
- */
-public class DuplicateAppointmentException extends DuplicateDataException {
-    public DuplicateAppointmentException() {
-        super("Operation would result in appointments clashes");
-    }
-}
